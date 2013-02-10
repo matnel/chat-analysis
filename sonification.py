53ec92ce8a04f0052ff80f774747102279e62a53
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-02-10 13:49:47 +0200

    Adding output for voices

--- sonification.py
+++ sonification.py
@@ -8,11 +8,21 @@ chord = {
 	-3: 'minormaj7',
 }
 
+def _voices( voices ):
+	if voices < 3:
+		return str( voices )
+	if voices < 5:
+		return '4'
+	if voices < 10:
+		return '5'
+	return '6'
+
 import math
 
 def sonify( data ):
-# delta, mean mood, this mood, mean weight, this weight
+# delta, mean mood, this mood, mean weight, this weight, voices
    print 'chordtype ' + chord[ round( data[1] ) ]
+   print 'voices ' + _voices( data[5] )
 
 ## initial
 print 'sound on'
