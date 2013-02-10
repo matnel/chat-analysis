a05e1f1a2de07b540d09f30f7e9dfa46ea295b17
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-02-10 13:58:16 +0200

    Fix mean calculation

--- extract.py
+++ extract.py
@@ -14,8 +14,8 @@ r = re.compile("\[sentence:(.*[0-9])\]")
 def _parse( line ):
    return map( lambda x: int(x), line.strip().split(',') )
 
-size = 3.0
-store = collections.deque(maxlen= size)
+SIZE = 3.0
+store = collections.deque(maxlen= SIZE)
 
 def _mood( positive, negative ):
   positive = positive - 1
@@ -26,12 +26,15 @@ def _mood( positive, negative ):
     return negative
 
 def analysis( data ):
-     store.append( data )
-     mood = reduce( lambda x, y : x + y, map( lambda x : _mood( x[1], x[2] ) , store ) )
-     weight = reduce( lambda x, y: x+y, map( lambda x : x[3], store ) )
-     voices = len( set( map( lambda x : x[4], store ) ) )
-     # delta, mean mood, this mood, mean weight, this weight, number of voices
-     return ( store[-1][0] - store[0][0] , mood / size , _mood( data[1], data[2] ) , weight / size, store[-1][3], voices )
+  store.append( data )
+
+  size = float( len( store ) )
+
+  mood = reduce( lambda x, y : x + y, map( lambda x : _mood( x[1], x[2] ) , store ) )
+  weight = reduce( lambda x, y: x+y, map( lambda x : x[3], store ) )
+  voices = len( set( map( lambda x : x[4], store ) ) )
+  # delta, mean mood, this mood, mean weight, this weight, number of voices
+  return ( store[-1][0] - store[0][0] , mood / size , _mood( data[1], data[2] ) , weight / size, store[-1][3], voices )
 
 def handle( line ):
    if not 'key' in line:
@@ -43,9 +46,15 @@ def handle( line ):
 	sentiment = urllib2.urlopen( http )
         t =  msg['time']
         out = []
+        ## 0 = MOOD
         out.append( datetime.strptime( t , '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%s') )
+        ## 1, 2 == SENTIMENT
         out.append( r.search( sentiment.read() ).group(1) )
+        ## 3 = message lenght
         out.append( str( len( msg['message'] ) ) )
+        print msg['message']
+        print len( msg['message'] )
+        ## 4 = voices
         out.append( str( hash( msg['from'] ) ) )
 	sonify( analysis( _parse( ','.join(out) ) ) )
 
