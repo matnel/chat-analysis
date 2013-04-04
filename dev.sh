88224530ae2b08aead06ded89fcea4f4f0febcd4
Matti Nelimarkka <mnelimar@chat.(none)>
2013-04-04 17:45:22 +0300

    helper scripts

--- dev.sh
+++ dev.sh
@@ -2,8 +2,8 @@ ping -c 1 foot 2> /dev/null 1> /dev/null
 
 if [ $? -eq 0 ] ; then
   echo 'direct'
-  ssh foot 'tail -f ~/mixer-pre/db/dev.db' | python extract.py
+  ssh mnelimar@foot 'tail -f ~/mixer-pre/db/dev.db' | python extract.py
 else
   echo 'via Shell'
-  ssh -t mnelimar@shell.hiit.fi ssh foot 'tail -f ~/mixer-pre/db/dev.db' | python extract.py
+  ssh -t mnelimar@shell.hiit.fi ssh foot 'tail -f ~/open/db/dev.db' | python extract.py
 fi
---
+++ ready.sh
@@ -0,0 +1,4 @@
+rm data
+touch data
+tail -f data | python ../musikalisaatio/output.py
+
